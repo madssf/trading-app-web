2d6cb664b4461afa472c3ecc327a3e910520cd58
madssf <mads.steenfeldt.foss@gmail.com>
2021-05-25 00:35:45 +0200

    add backend auth

---
+++ backend/server/apps/users/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url, include
+
+users_urlpatterns = [
+    url(r'^api/v1/', include('djoser.urls')),
+    url(r'^api/v1/', include('djoser.urls.authtoken')),
+]
--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.users.urls import users_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += users_urlpatterns
