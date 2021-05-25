5a1a067368d7c654c4133ec21d24be236e2f863a
madssf <mads.steenfeldt.foss@gmail.com>
2021-05-25 16:20:52 +0200

    add auth in react

--- frontend/src/Reducer.js
+++ frontend/src/Reducer.js
@@ -3,11 +3,14 @@ import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 
 import { signupReducer } from "./components/signup/SignupReducer";
+import { loginReducer } from "./components/login/LoginReducer";
+
 
 const createRootReducer = history =>
   combineReducers({
     router: connectRouter(history),
-    createUser: signupReducer
+    createUser: signupReducer,
+    auth: loginReducer
   });    
 
 export default createRootReducer;
--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,15 +1,12 @@
 // frontend/src/components/login/Login.js
 
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";     
 import { Link } from "react-router-dom";
-import {
-  Container,
-  Button,
-  Row,
-  Col,
-  Form,
-  FormControl
-} from "react-bootstrap";
+import {Container, Button, Row, Col, Form, FormControl} from "react-bootstrap";
+import { login } from "./LoginActions.js";
 
 class Login extends Component {
   constructor(props) {
@@ -28,7 +25,7 @@ class Login extends Component {
       username: this.state.username,
       password: this.state.password
     };
-    console.log("Login " + userData.username + " " + userData.password);
+    this.props.login(userData, "/dashboard");
   };
   render() {
     return (
@@ -46,7 +43,6 @@ class Login extends Component {
                   value={this.state.username}
                   onChange={this.onChange}
                 />
-                <FormControl.Feedback type="invalid"></FormControl.Feedback>
               </Form.Group>
 
               <Form.Group controlId="passwordId">
@@ -58,7 +54,6 @@ class Login extends Component {
                   value={this.state.password}
                   onChange={this.onChange}
                 />
-                <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
               </Form.Group>
             </Form>
             <Button color="primary" onClick={this.onLoginClick}>Login</Button>
@@ -72,4 +67,15 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, {
+  login
+})(withRouter(Login));
