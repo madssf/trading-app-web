eb314153f88dac09af3c0e1360d1c7ddac98a576
madssf <mads.steenfeldt.foss@gmail.com>
2021-07-10 00:33:52 +0200

    trying to sort

--- frontend/src/components/home/pages/currencies/CurrenciesList.js
+++ frontend/src/components/home/pages/currencies/CurrenciesList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
@@ -11,23 +11,87 @@ import Currency from './Currency'
 class CurrenciesList extends Component {
   componentDidMount() {
     this.props.getCurrencies();
+    
   }
 
  
   render() {
     const {currencies} = this.props.currencies
+    
     if (currencies.length === 0){
       return <p>No currencies yet.</p>
     }
-    let items = currencies.map(currency => {
-      return <Currency key={currency.id} currency={currency}/>;
-    })
+
+    let sortedCurrencies = [...currencies];
+    if (sortedCurrencies !== null) {
+      sortedCurrencies.sort((a,b) => {
+        if (a[this.state.sortedfield] < b[this.state.sortedfield]) {
+          return -1;
+        }
+        if (a[this.state.sortedfield] > b[this.state.sortedfield]) {
+          return 1;
+        }
+        return 0;
+      }) 
+    }
+   
+
     return (
           <Container>
           <div> 
           <h1>Currencies</h1>
           <p>Displaying {currencies.length} currencies</p>
-          {items}
+            <table>
+              <caption>Currencies</caption>
+ 
+            <thead>
+              <tr>
+              <th>
+                  <button type="button" onClick={() => this.setState({sortedfield: 'mcap_rank'})
+} >
+                    Rank
+                  </button>
+                </th>                
+                
+                
+                <th>
+                  <button type="button" onClick={() => this.setState({sortedfield: 'symbol'})
+} >
+                    Symbol
+                  </button>
+                </th>
+                <th>
+                  <button type="button" onClick={() => this.setState({sortedfield: 'name'})
+} >
+                    Name
+                  </button>
+                </th>               
+                <th>
+                  <button type="button" onClick={() => this.setState({sortedfield: 'last_price'})
+} >
+                    Price
+                  </button>
+                </th>                <th>
+                  <button type="button" onClick={() => this.setState({sortedfield: 'pct_change_24h'})
+} >
+                    % 24h
+                  </button>
+                </th>
+              </tr>
+            </thead>
+              <tbody>
+            {currencies.map(currency => (
+              <tr key={currency.id}>
+                <td>{currency.mcap_rank}</td>
+                <td>{currency.symbol}</td>
+                <td>{currency.name}</td>
+                <td>{currency.last_price}</td>
+                <td>{currency.pct_change_24h}</td>
+            
+              </tr>
+            ))}
+          </tbody>
+           </table>
           </div> 
           </Container>
 
@@ -46,3 +110,4 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps, {getCurrencies})(withRouter(CurrenciesList));
+
--- frontend/src/components/home/pages/currencies/Currency.js
+++ frontend/src/components/home/pages/currencies/Currency.js
@@ -10,7 +10,7 @@ class Currency extends Component {
       <div>
         <hr />
         <p>
-        <b>{currency.symbol}</b>: {currency.name}
+        <b>{currency.symbol}</b>: {currency.name} :  {currency.mcap_rank} : <b>{currency.last_price}</b> | {currency.pct_change_24h}
         </p>
       </div>
     );
