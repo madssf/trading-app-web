79f2c6e02adf2373e3677bce6bacac6946d9fae8
madssf <mads.steenfeldt.foss@gmail.com>
2021-08-24 18:59:26 +0200

    model bugfix

--- webserver/scripts/lambda_func.py
+++ webserver/scripts/lambda_func.py
@@ -9,14 +9,11 @@ def main():
     db.update_exchange_assets()
     portfolios = db.get_strategy_portfolios()
     instructions_list = []
-    emails_list = []
-    trades_list = []
     for portfolio in portfolios:
         if len(portfolio['assets']) > 0:
             strategy = MCAPRebalancer(
                 portfolio['strategy']['parameters'], portfolio['assets'])
             instructions = strategy.instruct(market)
-            
             if instructions:
                 email_notify =  sorted(x['symbol'] for x in instructions) != sorted(x['symbol'] for x in portfolio['strategy']['instructions']) and portfolio['email_notify'] if portfolio['strategy']['instructions'] else True
                 instructions_list.append({'id': portfolio['id'], 'email': portfolio['email'] if email_notify else False, 'execute_trades': portfolio['execute_trades'], 'instructions' : instructions, 'balanced_portfolio': strategy.balanced_portfolio, 'diff_matrix': strategy.diff_matrix})
